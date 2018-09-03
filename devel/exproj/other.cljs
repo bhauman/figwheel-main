1d4259a53c6f9b89251dfa8b8c75f1429c18ece6
Bruce Hauman <bhauman@gmail.com>
2018-09-03 14:48:54 -0400

    commit dev scratch file

--- devel/exproj/other.cljs
+++ devel/exproj/other.cljs
@@ -1,5 +1,24 @@
-(ns exproj.other)
+(ns exproj.other
+  (:require
+   [exproj.more]
+   [figwheel.main.async-result :as ar]
+   [figwheel.main.testing :refer-macros [run-tests run-tests-async]]
+   [cljs.test :refer-macros [deftest is async]]))
 
 (defn hello-there [] "hello there")
 
+(deftest hello
+  (is true))
+
+(defn -main [& args]
+  #_(js/setTimeout #(ar/throw-ex (ex-info "Man o man" {})) 3000)
+  #_(+ 1 2)
+  #_(throw  (ex-info "Campbells soup" {}))
+  #_[:figwheel.main.result/async-wait 5000]
+  #_(println "ARGS:" (pr-str args))
+
+  (run-tests-async 5000)
+  #_(run-tests)
+  #_(throw (ex-info "erorring oiut"  {}))
+  )
 
