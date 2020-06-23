46e2725396a8f48de1442481d43881db8e587223
Bruce Hauman <bhauman@gmail.com>
2020-06-23 14:35:03 -0600

    bump versions

--- project.clj
+++ project.clj
@@ -7,12 +7,12 @@
         :url "https://github.com/bhauman/figwheel-main"}
   :dependencies
   [[org.clojure/clojure "1.9.0"]
-   [org.clojure/clojurescript "1.10.339" :exclusions [commons-codec]]
+   [org.clojure/clojurescript "1.10.773" :exclusions [commons-codec]]
    [com.bhauman/figwheel-repl "0.2.10-SNAPSHOT"]
    [com.bhauman/figwheel-core "0.2.10-SNAPSHOT"]
    [com.bhauman/spell-spec "0.1.1"]
    [com.bhauman/cljs-test-display "0.1.1"]
-   [com.bhauman/certifiable "0.0.2"]
+   [com.bhauman/certifiable "0.0.3"]
    [ring "1.8.1"]
    [org.eclipse.jetty.websocket/websocket-servlet "9.4.28.v20200408"]
    [org.eclipse.jetty.websocket/websocket-server  "9.4.28.v20200408"]
