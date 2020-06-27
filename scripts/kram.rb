842608db10b1ae460350a5270306bf2736f79687
Bruce Hauman <bhauman@gmail.com>
2020-06-27 09:55:40 -0600

    update kramdown script

--- scripts/kram.rb
+++ scripts/kram.rb
@@ -1,12 +1,15 @@
 require 'kramdown'
+require 'kramdown-parser-gfm'
+require 'kramdown-syntax-coderay'
 
 for filename in ARGV do
   contents = File.read(filename)
   doc = Kramdown::Document.new(contents, {input: 'GFM',
                                           hard_wrap: false,
+                                          syntax_highlighter: :coderay,
                                           coderay_line_numbers: nil})
   new_name = filename.split("/").last.split(".").first + ".html"
-  content = "<!-- DO NOT EDIT: File generated from Markdown source found in #{filename} -->\n" + doc.to_html 
-  
+  content = "<!-- DO NOT EDIT: File generated from Markdown source found in #{filename} -->\n" + doc.to_html
+
   File.write('helper-resources/public/com/bhauman/figwheel/helper/content/' + new_name , content)
 end
