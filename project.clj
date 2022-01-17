acd03400dbcc4bfa5944a97a3da78655e3585eb7
Bruce Hauman <bhauman@gmail.com>
2022-01-17 10:36:45 -0600

    minor dev changes

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.bhauman/figwheel-main "0.2.15"
+(defproject com.bhauman/figwheel-main "0.2.16-SNAPSHOT"
   :description "Figwheel Main - Clojurescript tooling."
   :url "https://github.com/bhauman/figwheel-main"
   :license {:name "Eclipse Public License - v 1.0"
@@ -8,15 +8,15 @@
   :dependencies
   [[org.clojure/clojure "1.9.0"]
    [org.clojure/clojurescript "1.10.773" :exclusions [commons-codec]]
-   [com.bhauman/figwheel-repl "0.2.15"]
-   [com.bhauman/figwheel-core "0.2.15"]
+   [com.bhauman/figwheel-repl "0.2.16-SNAPSHOT"]
+   [com.bhauman/figwheel-core "0.2.16-SNAPSHOT"]
    [com.bhauman/spell-spec "0.1.1"]
    [com.bhauman/cljs-test-display "0.1.1"]
    [com.bhauman/certifiable "0.0.7"]
    [ring "1.9.1"]
    [org.eclipse.jetty.websocket/websocket-servlet "9.4.36.v20210114"]
    [org.eclipse.jetty.websocket/websocket-server  "9.4.36.v20210114"]
-   [binaryage/devtools "0.9.10"]
+   [binaryage/devtools "1.0.4"]
    [com.nextjournal/beholder "1.0.0"]
    [expound "0.7.1"]]
   :resource-paths ["helper-resources"]
