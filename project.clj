21f79094dcd4a9f7b959010081e0f154e5694717
Bruce Hauman <bhauman@gmail.com>
2022-05-16 10:52:03 -0600

    adding clojure.data.json and bumping to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.bhauman/figwheel-main "0.2.17"
+(defproject com.bhauman/figwheel-main "0.2.18-SNAPSHOT"
   :description "Figwheel Main - Clojurescript tooling."
   :url "https://github.com/bhauman/figwheel-main"
   :license {:name "Eclipse Public License - v 1.0"
@@ -6,10 +6,11 @@
   :scm {:name "git"
         :url "https://github.com/bhauman/figwheel-main"}
   :dependencies
-  [[org.clojure/clojure "1.9.0"]
+  [[org.clojure/clojure "1.10.0"]
    [org.clojure/clojurescript "1.10.773" :exclusions [commons-codec]]
-   [com.bhauman/figwheel-repl "0.2.17"]
-   [com.bhauman/figwheel-core "0.2.17"]
+   [org.clojure/data.json "2.4.0"]
+   [com.bhauman/figwheel-repl "0.2.18-SNAPSHOT"]
+   [com.bhauman/figwheel-core "0.2.18-SNAPSHOT"]
    [com.bhauman/spell-spec "0.1.1"]
    [com.bhauman/cljs-test-display "0.1.1"]
    [com.bhauman/certifiable "0.0.7"]
